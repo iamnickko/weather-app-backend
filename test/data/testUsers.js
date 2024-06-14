c0a7510c61afbe7128717ea8339db94feec8d20d
Nick Ko <nicholasedwardko@gmail.com>
2024-06-14 12:10:09 +0100

    feat: added mocha & chai test set up

---
+++ test/data/testUsers.js
@@ -0,0 +1,40 @@
+const testData = {
+  testUsers: [
+    {
+      _id: "66687296660ab591ab88e029",
+      name: "The Rock",
+      email: "paper@scissors.com",
+      password: "$2b$10$BoeSx9soIdh9sJCuJZ1ZBOmkZFEFgTpiiOc.722oWO0Zt7oNpOOQe",
+      savedLocations: [
+        {
+          coord: {
+            lat: 37.7021,
+            lon: -121.9358,
+          },
+          name: "Dublin",
+          apiId: 5344157,
+        },
+      ],
+      __v: 0,
+    },
+    {
+      _id: {
+        $oid: "6668729a660ab591ab88e02d",
+      },
+      name: "Valid Name",
+      email: "valid@email.com",
+      password: "$2b$10$1szlGDHgYtWn0Um8YVKo6.QixzAL9ezkmx2fH9tASx6IiKHuOMTQm",
+      savedLocations: [
+        {
+          name: "London",
+          apiId: 2643743,
+          coord: {
+            lat: 51.5085,
+            lon: -0.1257,
+          },
+        },
+      ],
+      __v: 0,
+    },
+  ],
+};
