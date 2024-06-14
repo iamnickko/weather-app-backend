2df1c9e935c4572db1cd8d0f691fd57c2f938a5d
Nick Ko <nicholasedwardko@gmail.com>
2024-06-14 22:33:17 +0100

    refactor: AuthController.login - thanks AI. added documentation img

--- src/services/Auth.services.js
+++ src/services/Auth.services.js
@@ -15,42 +15,43 @@ export default class AuthService {
     const { password, ...others } = newUser;
     try {
       const hashedPassword = bcrypt.hashSync(password, 10);
-      const newUser = await User.create({
+      const createdUser = await User.create({
         ...others,
         password: hashedPassword,
       });
       const accessToken = jwt.sign(
-        { id: newUser._id.toString() },
+        { id: createdUser._id.toString() },
         process.env.JWT_SECRET,
         { expiresIn: 86400 }
       );
-      return { accessToken, newUser };
+      return { accessToken, newUser: createdUser };
     } catch (error) {
       throw new Error(error);
     }
   };
 
   login = async (user) => {
-    try {
-      const dbUser = await User.findOne({ email: user.email });
-      const validPassword = bcrypt.compareSync(user.password, dbUser.password);
-      if (validPassword) {
-        const accessToken = jwt.sign(
-          { id: dbUser._id.toString() },
-          process.env.JWT_SECRET,
-          { expiresIn: 86400 }
-        );
-        return {
-          accessToken,
-          name: dbUser.name,
-          email: dbUser.email,
-          savedLocations: dbUser.savedLocations,
-        };
-      } else {
-        throw new Error("Invalid password.");
-      }
-    } catch (error) {
-      throw new Error(error);
+    const dbUser = await User.findOne({ email: user.email });
+    if (!dbUser) {
+      throw new Error("Invalid credentials.");
     }
+
+    const validPassword = await bcrypt.compare(user.password, dbUser.password);
+    if (!validPassword) {
+      throw new Error("Invalid credentials.");
+    }
+
+    const accessToken = jwt.sign(
+      { id: dbUser._id.toString() },
+      process.env.JWT_SECRET,
+      { expiresIn: 86400 }
+    );
+
+    return {
+      accessToken,
+      name: dbUser.name,
+      email: dbUser.email,
+      savedLocations: dbUser.savedLocations,
+    };
   };
 }
